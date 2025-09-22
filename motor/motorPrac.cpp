ed160b32d4ec496f2c0a1cb6efba238b1794a2fe
Luke <lbvido22@gmail.com>
2025-09-22 15:46:12 +0800

    Update motorPrac.cpp

--- motor/motorPrac.cpp
+++ motor/motorPrac.cpp
@@ -1,1 +1,22 @@
-#include
+//Missing Include files required
+//Should just spin a wheel straight up
+
+// Simple motor spin example
+
+const int motorPWM = 5;   // PWM pin connected to H-bridge motor speed input
+const int motorDir = 4;   // Direction pin connected to H-bridge
+
+void setup() {
+  pinMode(motorPWM, OUTPUT);
+  pinMode(motorDir, OUTPUT);
+
+  // Set motor direction forward
+  digitalWrite(motorDir, HIGH);
+
+  // Start motor at half speed (range 0-255)
+  analogWrite(motorPWM, 128);
+}
+
+void loop() {
+  // Nothing needed here, motor runs continuously
+}
